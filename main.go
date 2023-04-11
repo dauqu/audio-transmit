7fce02567e445f9d3a0737bc956de975cc517c08
Harsh Singh <sujeetsingh734321@gmail.com>
2023-04-11 17:24:41 +0530

    chnages

--- main.go
+++ main.go
@@ -1,129 +1,63 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
-	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
-} // WebSocket upgrader
-
-type Room struct {
-	ID           string
-	Clients      map[*websocket.Conn]bool
-	ClientsMutex sync.Mutex
 }
 
-var rooms map[string]*Room
-var roomsMutex sync.Mutex
-
-func createRoom(roomID string) {
-	roomsMutex.Lock()
-	defer roomsMutex.Unlock()
-
-	room := &Room{
-		ID:           roomID,
-		Clients:      make(map[*websocket.Conn]bool),
-		ClientsMutex: sync.Mutex{},
-	}
-	rooms[roomID] = room
-}
-
-func deleteRoom(roomID string) {
-	roomsMutex.Lock()
-	defer roomsMutex.Unlock()
-
-	delete(rooms, roomID)
-}
-
-func joinRoom(roomID string, conn *websocket.Conn) {
-	roomsMutex.Lock()
-	defer roomsMutex.Unlock()
-
-	room, ok := rooms[roomID]
-	if !ok {
-		log.Printf("Room '%s' not found\n", roomID)
-		return
-	}
-
-	room.ClientsMutex.Lock()
-	defer room.ClientsMutex.Unlock()
-
-	room.Clients[conn] = true
-	log.Printf("Client joined room '%s'\n", roomID)
-}
-
-func leaveRoom(roomID string, conn *websocket.Conn) {
-	roomsMutex.Lock()
-	defer roomsMutex.Unlock()
-
-	room, ok := rooms[roomID]
-	if !ok {
-		log.Printf("Room '%s' not found\n", roomID)
-		return
-	}
-
-	room.ClientsMutex.Lock()
-	defer room.ClientsMutex.Unlock()
-
-	delete(room.Clients, conn)
-	log.Printf("Client left room '%s'\n", roomID)
-}
+var clients = make(map[*websocket.Conn]bool) // connected clients
+var broadcast = make(chan []byte)           // channel for broadcasting messages
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	// Upgrade HTTP connection to WebSocket
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer conn.Close()
-
-	// Read room ID and join the room
-	_, roomID, err := conn.ReadMessage()
-	if err != nil {
-		log.Println(err)
+		log.Println("Failed to upgrade connection to WebSocket:", err)
 		return
 	}
 
-	joinRoom(string(roomID), conn)
+	clients[conn] = true // add client to connected clients map
 
-	// Read and write messages
 	for {
-		_, message, err := conn.ReadMessage()
+		// Read message from client
+		_, msg, err := conn.ReadMessage()
 		if err != nil {
-			log.Println(err)
+			log.Println("Failed to read message from client:", err)
+			delete(clients, conn)
 			break
 		}
 
-		// Broadcast message to all clients in the room
-		room, ok := rooms[string(roomID)]
-		if !ok {
-			log.Printf("Room '%s' not found\n", roomID)
-			break
-		}
+		// Broadcast message to all clients
+		broadcast <- msg
+	}
+}
 
-		room.ClientsMutex.Lock()
-		for client := range room.Clients {
-			if err := client.WriteMessage(websocket.TextMessage, message); err != nil {
-				log.Println(err)
-				continue
+func broadcastMessages() {
+	for {
+		// Get message from broadcast channel
+		msg := <-broadcast
+
+		// Send message to all connected clients
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				log.Println("Failed to write message to client:", err)
+				client.Close()
+				delete(clients, client)
 			}
 		}
-		room.ClientsMutex.Unlock()
 	}
-
-	leaveRoom(string(roomID), conn)
 }
 
 func main() {
-	rooms = make(map[string]*Room)
 	http.HandleFunc("/ws", handleWebSocket)
-	log.Println("Starting server on :8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	go broadcastMessages()
+	log.Fatal(http.ListenAndServe(":9000", nil))
 }
